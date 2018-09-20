5416ee78dcdc8831fd27cf483b0eef87dc7347f2
er-shubhendu-ee <38459865+er-shubhendu-ee@users.noreply.github.com>
2018-09-20 11:03:01 +0530

    working with id handeling

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 import socket
 
+
+quary_type_0='device_desc_0'
+quary_type_1='device_desc_1'
+quary_type_2='device_desc_2'
+action_id_0='action=0'
+action_id_1='action=1'
+action_id_2='action=2'
+
 HOST, PORT = '', 8888
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,27 +18,24 @@ print ('Serving HTTP on port %s ...' % PORT)
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(2048)
-    print (request)
-    for i in range(0,10):
-        print (request[i])
-    quary_type='device_desc'
-    quary_id_1='q=0'
-    quary_id_2='q=1'
-    if quary_type.encode('utf-8') in request:
-        http_response='true:device_desc'
-        if quary_id_1.encode('utf-8') in request:
-            http_response='at 0'
-            client_connection.sendall(http_response.encode('utf-8'))
-            client_connection.close()
-        elif quary_id_2.encode('utf-8') in request:
-            http_response='at 1'
-            client_connection.sendall(http_response.encode('utf-8'))
-            client_connection.close()
-        else:
-            http_response='out of bound'
-            client_connection.sendall(http_response.encode('utf-8'))
-            client_connection.close()
+    print (request,'\n')
+    if quary_type_0.encode('utf-8') in request:
+        http_response = 'device_desc_0 received'
+    elif quary_type_1.encode('utf-8') in request:
+        http_response = 'device_desc_1 received'
+    elif quary_type_2.encode('utf-8') in request:
+        http_response = 'device_desc_2 received'
     else:
-        http_response = 'false:device_desc'
-        client_connection.sendall(http_response.encode('utf-8'))
-        client_connection.close()
+        http_response = 'no device_desc received'
+
+    if action_id_0.encode('utf-8') in request:
+        http_response = http_response + ' ' + 'action_id_0 received'
+    elif action_id_1.encode('utf-8') in request:
+        http_response = http_response + ' ' + 'action_id_1 received'
+    elif action_id_2.encode('utf-8') in request:
+        http_response = http_response + ' ' + 'action_id_2 received'
+    else:
+        http_response = http_response + ' ' + 'no action received'    
+        
+    client_connection.sendall(http_response.encode('utf-8'))
+    client_connection.close()
